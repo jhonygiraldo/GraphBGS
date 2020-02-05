739d6796432c9e1f21b1724d3fbee5a231a099c9
Jhony Giraldo <jhonygiraldoz@gmail.com>
2020-02-05 16:55:21 +0100

    Add files via upload

---
+++ Experiments/compute_results/scripts/graph_results_pami_exp_3.m
@@ -0,0 +1,204 @@
+clear all, close all, clc;
+knn = load('../variational_splines-k-NN-k-30-R_50_FPN_COCO-median_filter/results_metrics.mat');
+robust = load('../variational_splines-robust-R_50_FPN_COCO-median_filter/results_metrics.mat');
+%%
+figures_cvpr = [1:11];
+challenges = {'badWeather';'baseline';'cameraJitter';'dynamicBackground';...
+    'intermittentObjectMotion';'lowFramerate';'nightVideos';'PTZ';'shadow';...
+    'thermal';'turbulence'};
+line_width = 1.5;
+marker_size = 6;
+font_size = 20;
+width = 680;
+heigth = 290;
+path_figures = 'figures_pami_exp_3/';
+mkdir(path_figures);
+%% Figure bad weather
+figure()
+errorbar(knn.sampling_density,mean(knn.average_FMeasure{figures_cvpr(1)}),std(knn.average_FMeasure{figures_cvpr(1)}),...
+    'LineWidth',line_width,'MarkerSize',marker_size);
+hold on;
+errorbar(robust.sampling_density,mean(robust.average_FMeasure{figures_cvpr(1)}),std(robust.average_FMeasure{figures_cvpr(1)}),...
+    'o--','LineWidth',line_width,'MarkerSize',marker_size);
+ylabel('Average f-measure','Interpreter','Latex');
+xlabel('Sampling density','Interpreter','Latex');
+xlim([knn.sampling_density(1) 0.1]);
+lgd = legend({'k-NN','Robust'},'Location','best');
+lgd.NumColumns = 2;
+set(lgd,'Interpreter','latex');
+set(lgd,'color','none');
+set(lgd,'Box','off');
+title('Bad Weather','Interpreter','Latex');
+get(gca);
+set(gca,'FontName','times','FontSize',font_size,'TickLabelInterpreter','Latex');
+set(gcf,'Position',[100,100,width,heigth]);
+saveas(gcf,[path_figures 'bad_weather.svg']);
+saveas(gcf,[path_figures 'bad_weather.png']);
+%% Figure baseline
+figure()
+errorbar(knn.sampling_density,mean(knn.average_FMeasure{figures_cvpr(2)}),std(knn.average_FMeasure{figures_cvpr(2)}),...
+    'LineWidth',line_width,'MarkerSize',marker_size);
+hold on;
+errorbar(robust.sampling_density,mean(robust.average_FMeasure{figures_cvpr(2)}),std(robust.average_FMeasure{figures_cvpr(2)}),...
+    'o--','LineWidth',line_width,'MarkerSize',marker_size);
+ylabel('Average f-measure','Interpreter','Latex');
+xlabel('Sampling density','Interpreter','Latex');
+xlim([knn.sampling_density(1) 0.1]);
+lgd = legend({'k-NN','Robust'},'Location','best');
+lgd.NumColumns = 2;
+set(lgd,'Interpreter','latex');
+set(lgd,'color','none');
+set(lgd,'Box','off');
+title('Baseline','Interpreter','Latex');
+get(gca);
+set(gca,'FontName','times','FontSize',font_size,'TickLabelInterpreter','Latex');
+set(gcf,'Position',[100,100,width,heigth]);
+saveas(gcf,[path_figures 'baseline.svg']);
+saveas(gcf,[path_figures 'baseline.png']);
+%% Figure camera jitter
+figure()
+errorbar(knn.sampling_density,mean(knn.average_FMeasure{figures_cvpr(3)}),std(knn.average_FMeasure{figures_cvpr(3)}),...
+    'LineWidth',line_width,'MarkerSize',marker_size);
+hold on;
+errorbar(robust.sampling_density,mean(robust.average_FMeasure{figures_cvpr(3)}),std(robust.average_FMeasure{figures_cvpr(3)}),...
+    'o--','LineWidth',line_width,'MarkerSize',marker_size);
+ylabel('Average f-measure','Interpreter','Latex');
+xlabel('Sampling density','Interpreter','Latex');
+xlim([knn.sampling_density(1) 0.1]);
+lgd = legend({'k-NN','Robust'},'Location','best');
+lgd.NumColumns = 2;
+set(lgd,'Interpreter','latex');
+set(lgd,'color','none');
+set(lgd,'Box','off');
+title('Camera Jitter','Interpreter','Latex');
+get(gca);
+set(gca,'FontName','times','FontSize',font_size,'TickLabelInterpreter','Latex');
+set(gcf,'Position',[100,100,width,heigth]);
+saveas(gcf,[path_figures 'camera_jitter.svg']);
+saveas(gcf,[path_figures 'camera_jitter.png']);
+%% Figure dynamic background
+figure()
+errorbar(knn.sampling_density,mean(knn.average_FMeasure{figures_cvpr(4)}),std(knn.average_FMeasure{figures_cvpr(4)}),...
+    'LineWidth',line_width,'MarkerSize',marker_size);
+hold on;
+errorbar(robust.sampling_density,mean(robust.average_FMeasure{figures_cvpr(4)}),std(robust.average_FMeasure{figures_cvpr(4)}),...
+    'o--','LineWidth',line_width,'MarkerSize',marker_size);
+ylabel('Average f-measure','Interpreter','Latex');
+xlabel('Sampling density','Interpreter','Latex');
+xlim([knn.sampling_density(1) 0.1]);
+lgd = legend({'k-NN','Robust'},'Location','best');
+lgd.NumColumns = 2;
+set(lgd,'Interpreter','latex');
+set(lgd,'color','none');
+set(lgd,'Box','off');
+title('Dynamic Background','Interpreter','Latex');
+get(gca);
+set(gca,'FontName','times','FontSize',font_size,'TickLabelInterpreter','Latex');
+set(gcf,'Position',[100,100,width,heigth]);
+saveas(gcf,[path_figures 'dynamic_background.svg']);
+saveas(gcf,[path_figures 'dynamic_background.png']);
+%% Figure intermittent object motion
+figure()
+errorbar(knn.sampling_density,mean(knn.average_FMeasure{figures_cvpr(5)}),std(knn.average_FMeasure{figures_cvpr(5)}),...
+    'LineWidth',line_width,'MarkerSize',marker_size);
+hold on;
+errorbar(robust.sampling_density,mean(robust.average_FMeasure{figures_cvpr(5)}),std(robust.average_FMeasure{figures_cvpr(5)}),...
+    'o--','LineWidth',line_width,'MarkerSize',marker_size);
+ylabel('Average f-measure','Interpreter','Latex');
+xlabel('Sampling density','Interpreter','Latex');
+xlim([knn.sampling_density(1) 0.1]);
+lgd = legend({'k-NN','Robust'},'Location','best');
+lgd.NumColumns = 2;
+set(lgd,'Interpreter','latex');
+set(lgd,'color','none');
+set(lgd,'Box','off');
+title('Intermittent Object Motion','Interpreter','Latex');
+get(gca);
+set(gca,'FontName','times','FontSize',font_size,'TickLabelInterpreter','Latex');
+set(gcf,'Position',[100,100,width,heigth]);
+saveas(gcf,[path_figures 'io_motion.svg']);
+saveas(gcf,[path_figures 'io_motion.png']);
+%% Figure low frame rate
+figure()
+errorbar(knn.sampling_density,mean(knn.average_FMeasure{figures_cvpr(6)}),std(knn.average_FMeasure{figures_cvpr(6)}),...
+    'LineWidth',line_width,'MarkerSize',marker_size);
+hold on;
+errorbar(robust.sampling_density,mean(robust.average_FMeasure{figures_cvpr(6)}),std(robust.average_FMeasure{figures_cvpr(6)}),...
+    'o--','LineWidth',line_width,'MarkerSize',marker_size);
+ylabel('Average f-measure','Interpreter','Latex');
+xlabel('Sampling density','Interpreter','Latex');
+xlim([knn.sampling_density(1) 0.1]);
+lgd = legend({'k-NN','Robust'},'Location','best');
+lgd.NumColumns = 2;
+set(lgd,'Interpreter','latex');
+set(lgd,'color','none');
+set(lgd,'Box','off');
+title('Low Frame Rate','Interpreter','Latex');
+get(gca);
+set(gca,'FontName','times','FontSize',font_size,'TickLabelInterpreter','Latex');
+set(gcf,'Position',[100,100,width,heigth]);
+saveas(gcf,[path_figures 'low_frame_rate.svg']);
+saveas(gcf,[path_figures 'low_frame_rate.png']);
+%% Figure PTZ
+figure()
+errorbar(knn.sampling_density,mean(knn.average_FMeasure{figures_cvpr(8)}),std(knn.average_FMeasure{figures_cvpr(8)}),...
+    'LineWidth',line_width,'MarkerSize',marker_size);
+hold on;
+errorbar(robust.sampling_density,mean(robust.average_FMeasure{figures_cvpr(8)}),std(robust.average_FMeasure{figures_cvpr(8)}),...
+    'o--','LineWidth',line_width,'MarkerSize',marker_size);
+ylabel('Average f-measure','Interpreter','Latex');
+xlabel('Sampling density','Interpreter','Latex');
+xlim([knn.sampling_density(1) 0.1]);
+lgd = legend({'k-NN','Robust'},'Location','best');
+lgd.NumColumns = 2;
+set(lgd,'Interpreter','latex');
+set(lgd,'color','none');
+set(lgd,'Box','off');
+title('PTZ','Interpreter','Latex');
+get(gca);
+set(gca,'FontName','times','FontSize',font_size,'TickLabelInterpreter','Latex');
+set(gcf,'Position',[100,100,width,heigth]);
+saveas(gcf,[path_figures 'ptz.svg']);
+saveas(gcf,[path_figures 'ptz.png']);
+%% Figure Shadow
+figure()
+errorbar(knn.sampling_density,mean(knn.average_FMeasure{figures_cvpr(9)}),std(knn.average_FMeasure{figures_cvpr(9)}),...
+    'LineWidth',line_width,'MarkerSize',marker_size);
+hold on;
+errorbar(robust.sampling_density,mean(robust.average_FMeasure{figures_cvpr(9)}),std(robust.average_FMeasure{figures_cvpr(9)}),...
+    'o--','LineWidth',line_width,'MarkerSize',marker_size);
+ylabel('Average f-measure','Interpreter','Latex');
+xlabel('Sampling density','Interpreter','Latex');
+xlim([knn.sampling_density(1) 0.1]);
+lgd = legend({'k-NN','Robust'},'Location','best');
+lgd.NumColumns = 2;
+set(lgd,'Interpreter','latex');
+set(lgd,'color','none');
+set(lgd,'Box','off');
+title('Shadow','Interpreter','Latex');
+get(gca);
+set(gca,'FontName','times','FontSize',font_size,'TickLabelInterpreter','Latex');
+set(gcf,'Position',[100,100,width,heigth]);
+saveas(gcf,[path_figures 'shadow.svg']);
+saveas(gcf,[path_figures 'shadow.png']);
+%% Figure Thermal
+figure()
+errorbar(knn.sampling_density,mean(knn.average_FMeasure{figures_cvpr(10)}),std(knn.average_FMeasure{figures_cvpr(10)}),...
+    'LineWidth',line_width,'MarkerSize',marker_size);
+hold on;
+errorbar(robust.sampling_density,mean(robust.average_FMeasure{figures_cvpr(10)}),std(robust.average_FMeasure{figures_cvpr(10)}),...
+    'o--','LineWidth',line_width,'MarkerSize',marker_size);
+ylabel('Average f-measure','Interpreter','Latex');
+xlabel('Sampling density','Interpreter','Latex');
+xlim([knn.sampling_density(1) 0.1]);
+lgd = legend({'k-NN','Robust'},'Location','best');
+lgd.NumColumns = 2;
+set(lgd,'Interpreter','latex');
+set(lgd,'color','none');
+set(lgd,'Box','off');
+title('Thermal','Interpreter','Latex');
+get(gca);
+set(gca,'FontName','times','FontSize',font_size,'TickLabelInterpreter','Latex');
+set(gcf,'Position',[100,100,width,heigth]);
+saveas(gcf,[path_figures 'thermal.svg']);
+saveas(gcf,[path_figures 'thermal.png']);
---
+++ Experiments/compute_results/scripts/graph_results_pami_exp_4.m
@@ -0,0 +1,246 @@
+clear all, close all, clc;
+TV = load('../TV-k-NN-k-30-R_50_FPN_COCO-median_filter/results_metrics.mat');
+e02 = load('../variational_splines-k-NN-k-30-R_50_FPN_COCO-median_filter/results_metrics.mat');
+e05 = load('../variational_splines-k-NN-k-30-R_50_FPN_COCO-median_filter-epsilon-0.5/results_metrics.mat');
+e50 = load('../variational_splines-k-NN-k-30-R_50_FPN_COCO-median_filter-epsilon-50/results_metrics.mat');
+%TV = e02;
+%%
+figures_cvpr = [1:11];
+challenges = {'badWeather';'baseline';'cameraJitter';'dynamicBackground';...
+    'intermittentObjectMotion';'lowFramerate';'nightVideos';'PTZ';'shadow';...
+    'thermal';'turbulence'};
+line_width = 1.5;
+marker_size = 6;
+font_size = 20;
+width = 680;
+heigth = 290;
+path_figures = 'figures_pami_exp_4/';
+mkdir(path_figures);
+%% Figure bad weather
+figure()
+errorbar(TV.sampling_density,mean(TV.average_FMeasure{figures_cvpr(1)}),std(TV.average_FMeasure{figures_cvpr(1)}),...
+    'LineWidth',line_width,'MarkerSize',marker_size);
+hold on;
+errorbar(e02.sampling_density,mean(e02.average_FMeasure{figures_cvpr(1)}),std(e02.average_FMeasure{figures_cvpr(1)}),...
+    'o--','LineWidth',line_width,'MarkerSize',marker_size);
+errorbar(e05.sampling_density,mean(e05.average_FMeasure{figures_cvpr(1)}),std(e05.average_FMeasure{figures_cvpr(1)}),...
+    '*-','LineWidth',line_width,'MarkerSize',marker_size);
+errorbar(e50.sampling_density,mean(e50.average_FMeasure{figures_cvpr(1)}),std(e50.average_FMeasure{figures_cvpr(1)}),...
+    'd-.','LineWidth',line_width,'MarkerSize',marker_size);
+ylabel('Average f-measure','Interpreter','Latex');
+xlabel('Sampling density','Interpreter','Latex');
+xlim([TV.sampling_density(1) 0.1]);
+lgd = legend({'TV','$\epsilon=0.2$','$\epsilon=0.5$','$\epsilon=50$'},'Location','best');
+lgd.NumColumns = 2;
+set(lgd,'Interpreter','latex');
+set(lgd,'color','none');
+set(lgd,'Box','off');
+title('Bad Weather','Interpreter','Latex');
+get(gca);
+set(gca,'FontName','times','FontSize',font_size,'TickLabelInterpreter','Latex');
+set(gcf,'Position',[100,100,width,heigth]);
+saveas(gcf,[path_figures 'bad_weather.svg']);
+saveas(gcf,[path_figures 'bad_weather.png']);
+%% Figure baseline
+figure()
+errorbar(TV.sampling_density,mean(TV.average_FMeasure{figures_cvpr(2)}),std(TV.average_FMeasure{figures_cvpr(2)}),...
+    'LineWidth',line_width,'MarkerSize',marker_size);
+hold on;
+errorbar(e02.sampling_density,mean(e02.average_FMeasure{figures_cvpr(2)}),std(e02.average_FMeasure{figures_cvpr(2)}),...
+    'o--','LineWidth',line_width,'MarkerSize',marker_size);
+errorbar(e05.sampling_density,mean(e05.average_FMeasure{figures_cvpr(2)}),std(e05.average_FMeasure{figures_cvpr(2)}),...
+    '*-','LineWidth',line_width,'MarkerSize',marker_size);
+errorbar(e50.sampling_density,mean(e50.average_FMeasure{figures_cvpr(2)}),std(e50.average_FMeasure{figures_cvpr(2)}),...
+    'd-.','LineWidth',line_width,'MarkerSize',marker_size);
+ylabel('Average f-measure','Interpreter','Latex');
+xlabel('Sampling density','Interpreter','Latex');
+xlim([TV.sampling_density(1) 0.1]);
+ylim([0.48 1]);
+lgd = legend({'TV','$\epsilon=0.2$','$\epsilon=0.5$','$\epsilon=50$'},'Location','best');
+lgd.NumColumns = 2;
+set(lgd,'Interpreter','latex');
+set(lgd,'color','none');
+set(lgd,'Box','off');
+title('Baseline','Interpreter','Latex');
+get(gca);
+set(gca,'FontName','times','FontSize',font_size,'TickLabelInterpreter','Latex');
+set(gcf,'Position',[100,100,width,heigth]);
+saveas(gcf,[path_figures 'baseline.svg']);
+saveas(gcf,[path_figures 'baseline.png']);
+%% Figure camera jitter
+figure()
+errorbar(TV.sampling_density,mean(TV.average_FMeasure{figures_cvpr(3)}),std(TV.average_FMeasure{figures_cvpr(3)}),...
+    'LineWidth',line_width,'MarkerSize',marker_size);
+hold on;
+errorbar(e02.sampling_density,mean(e02.average_FMeasure{figures_cvpr(3)}),std(e02.average_FMeasure{figures_cvpr(3)}),...
+    'o--','LineWidth',line_width,'MarkerSize',marker_size);
+errorbar(e05.sampling_density,mean(e05.average_FMeasure{figures_cvpr(3)}),std(e05.average_FMeasure{figures_cvpr(3)}),...
+    '*-','LineWidth',line_width,'MarkerSize',marker_size);
+errorbar(e50.sampling_density,mean(e50.average_FMeasure{figures_cvpr(3)}),std(e50.average_FMeasure{figures_cvpr(3)}),...
+    'd-.','LineWidth',line_width,'MarkerSize',marker_size);
+ylabel('Average f-measure','Interpreter','Latex');
+xlabel('Sampling density','Interpreter','Latex');
+xlim([TV.sampling_density(1) 0.1]);
+ylim([0.46 0.7]);
+lgd = legend({'TV','$\epsilon=0.2$','$\epsilon=0.5$','$\epsilon=50$'},'Location','best');
+lgd.NumColumns = 2;
+set(lgd,'Interpreter','latex');
+set(lgd,'color','none');
+set(lgd,'Box','off');
+title('Camera Jitter','Interpreter','Latex');
+get(gca);
+set(gca,'FontName','times','FontSize',font_size,'TickLabelInterpreter','Latex');
+set(gcf,'Position',[100,100,width,heigth]);
+saveas(gcf,[path_figures 'camera_jitter.svg']);
+saveas(gcf,[path_figures 'camera_jitter.png']);
+%% Figure dynamic background
+figure()
+errorbar(TV.sampling_density,mean(TV.average_FMeasure{figures_cvpr(4)}),std(TV.average_FMeasure{figures_cvpr(4)}),...
+    'LineWidth',line_width,'MarkerSize',marker_size);
+hold on;
+errorbar(e02.sampling_density,mean(e02.average_FMeasure{figures_cvpr(4)}),std(e02.average_FMeasure{figures_cvpr(4)}),...
+    'o--','LineWidth',line_width,'MarkerSize',marker_size);
+errorbar(e05.sampling_density,mean(e05.average_FMeasure{figures_cvpr(4)}),std(e05.average_FMeasure{figures_cvpr(4)}),...
+    '*-','LineWidth',line_width,'MarkerSize',marker_size);
+errorbar(e50.sampling_density,mean(e50.average_FMeasure{figures_cvpr(4)}),std(e50.average_FMeasure{figures_cvpr(4)}),...
+    'd-.','LineWidth',line_width,'MarkerSize',marker_size);
+ylabel('Average f-measure','Interpreter','Latex');
+xlabel('Sampling density','Interpreter','Latex');
+xlim([TV.sampling_density(1) 0.1]);
+lgd = legend({'TV','$\epsilon=0.2$','$\epsilon=0.5$','$\epsilon=50$'},'Location','best');
+lgd.NumColumns = 2;
+set(lgd,'Interpreter','latex');
+set(lgd,'color','none');
+set(lgd,'Box','off');
+title('Dynamic Background','Interpreter','Latex');
+get(gca);
+set(gca,'FontName','times','FontSize',font_size,'TickLabelInterpreter','Latex');
+set(gcf,'Position',[100,100,width,heigth]);
+saveas(gcf,[path_figures 'dynamic_background.svg']);
+saveas(gcf,[path_figures 'dynamic_background.png']);
+%% Figure intermittent object motion
+figure()
+errorbar(TV.sampling_density,mean(TV.average_FMeasure{figures_cvpr(5)}),std(TV.average_FMeasure{figures_cvpr(5)}),...
+    'LineWidth',line_width,'MarkerSize',marker_size);
+hold on;
+errorbar(e02.sampling_density,mean(e02.average_FMeasure{figures_cvpr(5)}),std(e02.average_FMeasure{figures_cvpr(5)}),...
+    'o--','LineWidth',line_width,'MarkerSize',marker_size);
+errorbar(e05.sampling_density,mean(e05.average_FMeasure{figures_cvpr(5)}),std(e05.average_FMeasure{figures_cvpr(5)}),...
+    '*-','LineWidth',line_width,'MarkerSize',marker_size);
+errorbar(e50.sampling_density,mean(e50.average_FMeasure{figures_cvpr(5)}),std(e50.average_FMeasure{figures_cvpr(5)}),...
+    'd-.','LineWidth',line_width,'MarkerSize',marker_size);
+ylabel('Average f-measure','Interpreter','Latex');
+xlabel('Sampling density','Interpreter','Latex');
+xlim([TV.sampling_density(1) 0.1]);
+lgd = legend({'TV','$\epsilon=0.2$','$\epsilon=0.5$','$\epsilon=50$'},'Location','best');
+lgd.NumColumns = 2;
+set(lgd,'Interpreter','latex');
+set(lgd,'color','none');
+set(lgd,'Box','off');
+title('Intermittent Object Motion','Interpreter','Latex');
+get(gca);
+set(gca,'FontName','times','FontSize',font_size,'TickLabelInterpreter','Latex');
+set(gcf,'Position',[100,100,width,heigth]);
+saveas(gcf,[path_figures 'io_motion.svg']);
+saveas(gcf,[path_figures 'io_motion.png']);
+%% Figure low frame rate
+figure()
+errorbar(TV.sampling_density,mean(TV.average_FMeasure{figures_cvpr(6)}),std(TV.average_FMeasure{figures_cvpr(6)}),...
+    'LineWidth',line_width,'MarkerSize',marker_size);
+hold on;
+errorbar(e02.sampling_density,mean(e02.average_FMeasure{figures_cvpr(6)}),std(e02.average_FMeasure{figures_cvpr(6)}),...
+    'o--','LineWidth',line_width,'MarkerSize',marker_size);
+errorbar(e05.sampling_density,mean(e05.average_FMeasure{figures_cvpr(6)}),std(e05.average_FMeasure{figures_cvpr(6)}),...
+    '*-','LineWidth',line_width,'MarkerSize',marker_size);
+errorbar(e50.sampling_density,mean(e50.average_FMeasure{figures_cvpr(6)}),std(e50.average_FMeasure{figures_cvpr(6)}),...
+    'd-.','LineWidth',line_width,'MarkerSize',marker_size);
+ylabel('Average f-measure','Interpreter','Latex');
+xlabel('Sampling density','Interpreter','Latex');
+xlim([TV.sampling_density(1) 0.1]);
+lgd = legend({'TV','$\epsilon=0.2$','$\epsilon=0.5$','$\epsilon=50$'},'Location','best');
+lgd.NumColumns = 2;
+set(lgd,'Interpreter','latex');
+set(lgd,'color','none');
+set(lgd,'Box','off');
+title('Low Frame Rate','Interpreter','Latex');
+get(gca);
+set(gca,'FontName','times','FontSize',font_size,'TickLabelInterpreter','Latex');
+set(gcf,'Position',[100,100,width,heigth]);
+saveas(gcf,[path_figures 'low_frame_rate.svg']);
+saveas(gcf,[path_figures 'low_frame_rate.png']);
+%% Figure PTZ
+figure()
+errorbar(TV.sampling_density,mean(TV.average_FMeasure{figures_cvpr(8)}),std(TV.average_FMeasure{figures_cvpr(8)}),...
+    'LineWidth',line_width,'MarkerSize',marker_size);
+hold on;
+errorbar(e02.sampling_density,mean(e02.average_FMeasure{figures_cvpr(8)}),std(e02.average_FMeasure{figures_cvpr(8)}),...
+    'o--','LineWidth',line_width,'MarkerSize',marker_size);
+errorbar(e05.sampling_density,mean(e05.average_FMeasure{figures_cvpr(8)}),std(e05.average_FMeasure{figures_cvpr(8)}),...
+    '*-','LineWidth',line_width,'MarkerSize',marker_size);
+errorbar(e50.sampling_density,mean(e50.average_FMeasure{figures_cvpr(8)}),std(e50.average_FMeasure{figures_cvpr(8)}),...
+    'd-.','LineWidth',line_width,'MarkerSize',marker_size);
+ylabel('Average f-measure','Interpreter','Latex');
+xlabel('Sampling density','Interpreter','Latex');
+xlim([TV.sampling_density(1) 0.1]);
+lgd = legend({'TV','$\epsilon=0.2$','$\epsilon=0.5$','$\epsilon=50$'},'Location','best');
+lgd.NumColumns = 2;
+set(lgd,'Interpreter','latex');
+set(lgd,'color','none');
+set(lgd,'Box','off');
+title('PTZ','Interpreter','Latex');
+get(gca);
+set(gca,'FontName','times','FontSize',font_size,'TickLabelInterpreter','Latex');
+set(gcf,'Position',[100,100,width,heigth]);
+saveas(gcf,[path_figures 'ptz.svg']);
+saveas(gcf,[path_figures 'ptz.png']);
+%% Figure Shadow
+figure()
+errorbar(TV.sampling_density,mean(TV.average_FMeasure{figures_cvpr(9)}),std(TV.average_FMeasure{figures_cvpr(9)}),...
+    'LineWidth',line_width,'MarkerSize',marker_size);
+hold on;
+errorbar(e02.sampling_density,mean(e02.average_FMeasure{figures_cvpr(9)}),std(e02.average_FMeasure{figures_cvpr(9)}),...
+    'o--','LineWidth',line_width,'MarkerSize',marker_size);
+errorbar(e05.sampling_density,mean(e05.average_FMeasure{figures_cvpr(9)}),std(e05.average_FMeasure{figures_cvpr(9)}),...
+    '*-','LineWidth',line_width,'MarkerSize',marker_size);
+errorbar(e50.sampling_density,mean(e50.average_FMeasure{figures_cvpr(9)}),std(e50.average_FMeasure{figures_cvpr(9)}),...
+    'd-.','LineWidth',line_width,'MarkerSize',marker_size);
+ylabel('Average f-measure','Interpreter','Latex');
+xlabel('Sampling density','Interpreter','Latex');
+xlim([TV.sampling_density(1) 0.1]);
+lgd = legend({'TV','$\epsilon=0.2$','$\epsilon=0.5$','$\epsilon=50$'},'Location','best');
+lgd.NumColumns = 2;
+set(lgd,'Interpreter','latex');
+set(lgd,'color','none');
+set(lgd,'Box','off');
+title('Shadow','Interpreter','Latex');
+get(gca);
+set(gca,'FontName','times','FontSize',font_size,'TickLabelInterpreter','Latex');
+set(gcf,'Position',[100,100,width,heigth]);
+saveas(gcf,[path_figures 'shadow.svg']);
+saveas(gcf,[path_figures 'shadow.png']);
+%% Figure Thermal
+figure()
+errorbar(TV.sampling_density,mean(TV.average_FMeasure{figures_cvpr(10)}),std(TV.average_FMeasure{figures_cvpr(10)}),...
+    'LineWidth',line_width,'MarkerSize',marker_size);
+hold on;
+errorbar(e02.sampling_density,mean(e02.average_FMeasure{figures_cvpr(10)}),std(e02.average_FMeasure{figures_cvpr(10)}),...
+    'o--','LineWidth',line_width,'MarkerSize',marker_size);
+errorbar(e05.sampling_density,mean(e05.average_FMeasure{figures_cvpr(10)}),std(e05.average_FMeasure{figures_cvpr(10)}),...
+    '*-','LineWidth',line_width,'MarkerSize',marker_size);
+errorbar(e50.sampling_density,mean(e50.average_FMeasure{figures_cvpr(10)}),std(e50.average_FMeasure{figures_cvpr(10)}),...
+    'd-.','LineWidth',line_width,'MarkerSize',marker_size);
+ylabel('Average f-measure','Interpreter','Latex');
+xlabel('Sampling density','Interpreter','Latex');
+xlim([TV.sampling_density(1) 0.1]);
+ylim([0.46 0.72]);
+lgd = legend({'TV','$\epsilon=0.2$','$\epsilon=0.5$','$\epsilon=50$'},'Location','best');
+lgd.NumColumns = 2;
+set(lgd,'Interpreter','latex');
+set(lgd,'color','none');
+set(lgd,'Box','off');
+title('Thermal','Interpreter','Latex');
+get(gca);
+set(gca,'FontName','times','FontSize',font_size,'TickLabelInterpreter','Latex');
+set(gcf,'Position',[100,100,width,heigth]);
+saveas(gcf,[path_figures 'thermal.svg']);
+saveas(gcf,[path_figures 'thermal.png']);
